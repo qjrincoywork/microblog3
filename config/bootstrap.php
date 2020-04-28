cf2366de39fe5a89e717600cd2071052e4cbe1bd
qjrincoywork <quirjohnincoy.work@gmail.com>
2020-04-28 14:34:44 +0800

    email configurations

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -87,7 +87,7 @@ try {
  * When debug = true the metadata cache should only last
  * for a short time.
  */
-Configure::write('debug', 2);
+Configure::write('debug', 0);
 if (Configure::read('debug')) {
     // Configure::write('DebugKit.forceEnable', TRUE);
     // Plugin::addPlugin('DebugKit', ['bootstrap' => TRUE]);
